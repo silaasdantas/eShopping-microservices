1b88f1cea4c6f2c059e2e70221c8985575506e35
Silas Dantas Pereira <silas-dantas@hotmail.com>
2024-08-14 01:15:19 -0300

    Implementing Lazy Mapper

---
+++ Catalog/Catalog.Application/Mappers/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Catalog.Application.Mappers;
+
+public static class Mapper
+{
+    private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+            cfg.AddProfile<ProductMappingProfile>();
+        });
+        var mapper = config.CreateMapper();
+        return mapper;
+    });
+
+    public static IMapper MapperExt => Lazy.Value;
+}

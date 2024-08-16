f77845c46d2c661235503b0d987ac2f4d84dd909
Silas Dantas <silas-dantas@hotmail.com>
2024-08-16 02:14:58 -0300

    configs to Docker Compose

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using Catalog.Core.Repositories;
 using Catalog.Infrastructure.Data;
 using Catalog.Infrastructure.Repositories;
@@ -8,9 +9,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+//Add API Versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(opt => opt.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Catalog.API", Version = "v1" }));
+builder.Services.AddSwaggerGen(opt =>
+opt.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Catalog.API", Version = "v1" }));
+
 //Register AutoMapper
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
